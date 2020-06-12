6d34ee9f592bfa139e27a0d064cd821a665eef50
Olivier Lecluse <olivier.lecluse@free.fr>
2020-06-12 13:33:19 +0200

    setup MSI

---
+++ setup.py
@@ -0,0 +1,22 @@
+import sys
+from cx_Freeze import setup, Executable
+
+# Dependencies are automatically detected, but it might need fine tuning.
+build_exe_options = {
+    "packages": ["os", "pyside2", "serial"],
+    "include_files": ["cli/", "assets/", "examples/", "src/", "README.md", "LICENSE"],
+    "excludes": []
+}
+
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+    if 'bdist_msi' in sys.argv:
+        sys.argv += ['--initial-target-dir', 'c:\DigiQt']
+
+setup(  name = "DigiQt",
+        version = "0.2 beta",
+        description = "Digirule2 assembler and simulator",
+        author="Olivier Lecluse - Thomas Lecluse",
+        options = {"build_exe": build_exe_options},
+        executables = [Executable("main.py", base=base)])
