8646f38ab6b32fff2bbc0ee1e3540909f8e397b7
Olivier Lecluse <olivier.lecluse@free.fr>
2020-06-12 19:35:47 +0200

    optimization :)

--- examples/test_int2str.asm
+++ examples/test_int2str.asm
@@ -1,19 +1,19 @@
 %define 	ZFlag 0
 %define 	statusReg 252
 
-copylr	153 r0
+copylr	53 r0
 
 //
 // converting binary to decimal
 //
 // Initialize the stack pointer to the second element
-copylr	stack+1 stackPtr	
+copylr	stack stackPtr	
   
 :get_digits                 
     div        r0 ten
     addla    '0'
-    incr    stackPtr
     copyai    stackPtr
+    incr    stackPtr
     copyra    r0
     bcrss    ZFlag statusReg
     jump    get_digits  
@@ -21,27 +21,13 @@ copylr	stack+1 stackPtr
     halt
     
 // The END
-// Result is in stack+1
+// Result is in stack+4
 // 
 
-//
-// Implementing stack
-//
-
-// pushes accumulator into the stack
-:push
-    copyai	stackPtr
-    incr	stackPtr
-    return
-// pops a number out of the stack into the acc
-:pop
-    decr	stackPtr
-    copyia	stackPtr
-    return
 
 // stack variables
 %data 	r0 0
 %data 	ten 10
 %data 	stackPtr 0
 // Stack initialized with NULL caracters
-%data 	stack 0 0 0 0
+%data 	stack 0 0 0 255
